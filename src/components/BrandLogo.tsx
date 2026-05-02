d0ad9826dbeb70ff0073c9afa3790300d3c4e4eb
OragonNugget <dnugget143@gmail.com>
2026-05-02 09:55:11 +0800

    logos fix

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -4,12 +4,7 @@ type BrandLogoProps = {
 };
 
 export default function BrandLogo({ className = "w-9 h-9", alt = "AcadNest brand logo" }: BrandLogoProps) {
-  // Use favicon.svg as the always-available source, but support favicon.png if you drop it into `public/`.
   return (
-    <picture>
-      <source srcSet="/favicon.png" type="image/png" />
-      <img src="/favicon.svg" alt={alt} className={className} draggable={false} />
-    </picture>
+    <img src="/favicon.svg" alt={alt} className={className} draggable={false} />
   );
 }
-
